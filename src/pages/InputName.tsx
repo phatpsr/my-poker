cc16921bb9f72ca0066316901836f3160e91518a
Phat <phat.psr@gmail.com>
2024-05-09 19:33:28 +0700

    Add navigate to next page

--- src/pages/InputName.tsx
+++ src/pages/InputName.tsx
@@ -1,25 +1,34 @@
+import { useNavigate } from "react-router-dom";
+
 function InputName() {
+  const navigate = useNavigate();
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    navigate("/poker")
+  };
   return (
     <div className="container mt-5">
       <h1>My Poker</h1>
-      <div className="row g-3 align-items-center">
-        <div className="col-auto">
-          <span className="col-form-label">Name</span>
+      <form onSubmit={handleSubmit}>
+        <div className="row g-3 align-items-center">
+          <div className="col-auto">
+            <span className="col-form-label">Name</span>
+          </div>
+          <div className="col-auto">
+            <input
+              type="text"
+              id="name"
+              className="form-control"
+              placeholder="Input your name"
+            />
+          </div>
+          <div className="col-auto">
+            <button type="submit" className="btn btn-primary">
+              Next
+            </button>
+          </div>
         </div>
-        <div className="col-auto">
-          <input
-            type="text"
-            id="name"
-            className="form-control"
-            placeholder="Input your name"
-          />
-        </div>
-        <div className="col-auto">
-          <button type="submit" className="btn btn-primary">
-            Next
-          </button>
-        </div>
-      </div>
+      </form>
     </div>
   );
 }
